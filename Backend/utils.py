10ecdac47e0420e16b7110c7dedf3a45e4bc7638
Gudauu <78345565+Gudauu@users.noreply.github.com>
2024-02-12 13:12:41 -0500

    Check empty environment variables & Reformat .env.example (#198)
    * reformat .env.example: removed inline comments due to parsing error in load_dotenv
    
    * Add empty environment variable checks

--- Backend/utils.py
+++ Backend/utils.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import json
 import random
 import logging
@@ -90,3 +91,28 @@ def choose_random_song() -> str:
         return f"../Songs/{song}"
     except Exception as e:
         logger.error(colored(f"Error occurred while choosing random song: {str(e)}", "red"))
+
+
+def check_env_vars() -> None:
+    """
+    Checks if the necessary environment variables are set.
+
+    Returns:
+        None
+
+    Raises:
+        SystemExit: If any required environment variables are missing.
+    """
+    try:
+        required_vars = ["PEXELS_API_KEY", "TIKTOK_SESSION_ID", "IMAGEMAGICK_BINARY"]
+        missing_vars = [var + os.getenv(var)  for var in required_vars if os.getenv(var) is None or (len(os.getenv(var)) == 0)]  
+
+        if missing_vars:
+            missing_vars_str = ", ".join(missing_vars)
+            logger.error(colored(f"The following environment variables are missing: {missing_vars_str}", "red"))
+            logger.error(colored("Please consult 'EnvironmentVariables.md' for instructions on how to set them.", "yellow"))
+            sys.exit(1)  # Aborts the program
+    except Exception as e:
+        logger.error(f"Error occurred while checking environment variables: {str(e)}")
+        sys.exit(1)  # Aborts the program if an unexpected error occurs
+
